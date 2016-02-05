a8f3b5a8e0c9edb35f0542a66d9f2b3b2d269e58
imBulbi <mlggrizz3@gmail.com>
2016-02-04 22:35:37 -0500

    Add my version of LargeSum

--- LargeSum.java
+++ LargeSum.java
@@ -9,19 +9,41 @@
 import java.io.*;
 
 public class LargeSum {
-	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+	public static void main(String[] args) throws IOException{
 		int sum = 0;
+		String[] myArr = {"0", "0", "0", "0"};
+		BufferedReader br = new BufferedReader(new FileReader("input.txt"));
 		try {
-			String line = br.readLine();
-			StringBuilder sb = new StringBuilder();
+			String line;
+			int lenStr;
 
-			while (line != null) {
-				line = br.readLine();
+			while ((line = br.readLine()) != null) {
+				int index = 3;
+				String carry = "0";
+				lenStr = line.length();
+				while ((lenStr - 16) >= 0) {
+					String subStr = String.valueOf(Long.parseLong(myArr[index]) + 
+									Long.parseLong(line.substring(lenStr - 16, lenStr)) +
+									Long.parseLong(carry));
+					int lenSubStr = subStr.length();
+					if (lenSubStr > 16) {
+						carry = subStr.substring(0, 1);
+						myArr[index--] = subStr.substring(1, lenSubStr);
+					} else {
+						myArr[index--] = subStr;
+					}
+					lenStr -= 16;
+					//TODO: Do memory size check for carry over?
+				}
+				if (lenStr > 0) {
+					myArr[index] = String.valueOf(Long.parseLong(myArr[index]) + 
+									 Long.parseLong(line.substring(0, lenStr)));
+				}
+				System.out.println(myArr[0] + " " + myArr[1] + " " + myArr[2] + " " + myArr[3]);
 			}
-			System.out.println(sb.toString());
 		} finally {
 			br.close();
 		}
+		System.out.println(myArr[3]);
 	}
 }

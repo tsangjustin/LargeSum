9be2e86967aa16b1a614ca1bfb6ae30fd7e18939
Mike Curry <mjcurry@users.noreply.github.com>
2016-02-05 07:56:51 -0500

    Final tweaks

--- mike.java
+++ mike.java
@@ -1,3 +1,4 @@
+package com.company;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -12,7 +13,6 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-
         Stack<Byte> digitStack = new Stack<Byte>();
         BufferedReader br = new BufferedReader(new FileReader("input.txt"));
 
@@ -67,4 +67,5 @@ public class Main {
             System.out.print(digitStack.get(i));
         }
     }
+
 }
